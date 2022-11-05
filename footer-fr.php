4376d05dabc960d127cbc6560d6b5fd0185710ea
Niksune <47556429+Niksune@users.noreply.github.com>
2022-11-05 15:05:04 +0100

    Normalisation footer

--- footer-fr.php
+++ footer-fr.php
@@ -7,9 +7,9 @@
 					<h5>À propos de Fauntastic</h5>
 					<hr>
 					<p>Fauntastic 2023 - Wild in the Street, est la troisième édition de votre sympathique convention furry lyonnaise. Nous mettons l'accent sur l'amitié, le partage, la créativité et le fun. Des valeurs dont notre communauté peut être fière. Il n'est pas nécessaire d'être furry pour pouvoir se joindre à nous !</p>
-                    <a href="<?= get_permalink( get_page_by_path('a-propos')); ?>" class="hover-blue">À Propos de Nous...</a><br />
-                    <a href="<?= get_permalink(get_page_by_path('code-de-conduite')) ?>">Code de Conduite</a><br />
-                    <a href="<?= get_permalink(get_page_by_path('cookie-policy-eu')) ?>" class="hover-blue">Politique de Cookies</a><br />
+                    <a href="<?= get_permalink( get_page_by_path('a-propos')); ?>" class="hover-blue">À propos de nous</a><br />
+                    <a href="<?= get_permalink(get_page_by_path('code-de-conduite')) ?>">Code de conduite</a><br />
+                    <a href="<?= get_permalink(get_page_by_path('cookie-policy-eu')) ?>" class="hover-blue">Politique de cookies</a><br />
 				</article>
 				<article class="contact-w">
 					<h5>Nous contacter</h5>
@@ -17,13 +17,13 @@
 					<ul class="contact-items">
 						<li>
 							<img src="<?php echo get_template_directory_uri();?>/assets/images/fauntastic-icon-telegram.svg" alt="ctc-option">
-							<h6>Telegram:</h6>
+							<h6>Telegram :</h6>
                             <p><a href="https://t.me/FauntasticChat">t.me/FauntasticChat</a></p>
                             <p><a href="https://t.me/FauntasticCon">t.me/FauntasticCon</a></p>
 						</li>
 						<li>
 							<img src="<?php echo get_template_directory_uri();?>/assets/images/fauntastic-icon-mail-outline.svg" alt="ctc-option">
-							<h6>Mail:</h6>
+							<h6>Mail :</h6>
 							<p><a href="mailto:contact@fauntastic.eu">contact@fauntastic.eu</p>
 							<p><a href="mailto:registration@fauntastic.eu">registration@fauntastic.eu</p>
 							<p><a href="mailto:volunteers@fauntastic.eu">volunteers@fauntastic.eu</p>
