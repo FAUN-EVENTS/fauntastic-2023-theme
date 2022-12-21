917ecf66bbd70a3db9ec441bbfcc621a1e9fc775
vanilor <services+gitlab@vanilor.net>
2022-12-21 19:39:15 +0100

    CSS bug fix on 404

--- en/404-en.php
+++ en/404-en.php
@@ -3,7 +3,7 @@
 <section>
 
     <img src="<?= get_template_directory_uri() ?>/assets/images/404.png" alt="404 Not Found :["
-         style="max-width: 400px; display: block; margin-left: auto; margin-right: auto"
+         style="max-width: 100%; width: 400px; display: block; margin-left: auto; margin-right: auto"
     >
     <p style="text-transform: capitalize; text-align: center">Uhoh...</p>
     <p style="text-align: center">The page you requested was not found</p>
